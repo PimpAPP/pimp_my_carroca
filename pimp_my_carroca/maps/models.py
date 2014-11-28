740b8b85a279bd8cea51aa30a7492a7e144b1802
Vido <vido@usp.br>
2014-11-28 21:28:49 -0200

    Basic project structure.
    Just for sync!

---
+++ pimp_my_carroca/maps/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+# Create your models here.
+class Points(models.Model):
+    pass
---
+++ pimp_my_carroca/maps/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def show_maps(request):
+    template = "maps/show_map.html"
+    return
+
+def new_carroca(request):
+    template = "maps/new_carroca.html"
+    return
