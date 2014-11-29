8137a763cfd666d87799ef4fbb0f156094ec4240
Vido <vido@usp.br>
2014-11-28 22:37:59 -0200

    Error free

--- pimp_my_carroca/maps/views.py
+++ pimp_my_carroca/maps/views.py
@@ -1,4 +1,6 @@
+from django.shortcuts import render_to_response
 from django.shortcuts import render
+from django.template import RequestContext
 
 # Create your views here.
 
--- pimp_my_carroca/pimp_my_carroca/urls.py
+++ pimp_my_carroca/pimp_my_carroca/urls.py
@@ -3,13 +3,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import maps
-
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'pimp_my_carroca.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^', include('maps.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^maps/', include(maps.urls)),
 )
