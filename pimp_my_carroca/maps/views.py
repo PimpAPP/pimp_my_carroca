a55f9c12a158e0fa53b4d7d32c755ae5d5142616
Alexandre Souza <alexandre_@fastmail.com>
2014-11-29 03:36:04 -0200

    pep8

--- pimp_my_carroca/maps/views.py
+++ pimp_my_carroca/maps/views.py
@@ -1,23 +1,26 @@
 from django.shortcuts import render_to_response
-from django.shortcuts import render
 from django.template import RequestContext
 
 # Create your views here.
+
 
 def hallo(request):
     template = "hello.html"
     return render_to_response(
         template, {}, context_instance=RequestContext(request))
 
+
 def show_maps(request):
     template = "maps/show_map.html"
     return render_to_response(
         template, {}, context_instance=RequestContext(request))
 
+
 def new_form(request):
     template = "maps/index.html"
     return render_to_response(
         template, {}, context_instance=RequestContext(request))
+
 
 def form_receiver(request):
     template = "maps/new_carroca.html"
