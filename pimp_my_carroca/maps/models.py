08a8013c5e7d0a881a8cb136d35b789b6523d5ff
Vido <vido@usp.br>
2014-11-28 22:08:51 -0200

    Some hallo world code Just for sync

--- pimp_my_carroca/maps/models.py
+++ pimp_my_carroca/maps/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Points(models.Model):
-    pass
+   point_id = models.IntegerField()
+   nome = models.TextField()
+   telefone = models.TextField()
+   latitude = models.DecimalField(decimal_places=8, max_digits=11)
+   longitude =  models.DecimalField(decimal_places=8, max_digits=11)
+   tipo = models.TextField()
--- pimp_my_carroca/maps/views.py
+++ pimp_my_carroca/maps/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def hallo(request):
+    template = "hello.html"
+    return render_to_response(
+        template, {}, context_instance=RequestContext(request))
+
 def show_maps(request):
     template = "maps/show_map.html"
-    return
+    return render_to_response(
+        template, {}, context_instance=RequestContext(request))
 
 def new_carroca(request):
     template = "maps/new_carroca.html"
-    return
+    return render_to_response(
+        template, {}, context_instance=RequestContext(request))
